e6e62a3bdc5047bc2da9f4a9e16c26db38aa7c60
Sk Faisal <sk7050@localhost.localdomain>
2021-01-16 09:54:04 -0800

    1st commit

---
+++ test.py
@@ -0,0 +1,8 @@
+a=int(input("please press a number"))
+b=int(input("please press a number"))
+if a>b:
+   print("a is bigger then b")
+if b>a:
+   print("b is bigger then a")
+else:
+   print("a & b is same")
